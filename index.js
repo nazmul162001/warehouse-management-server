7de509dc2ad7fd5539012523fd5e654a2ce3019c
nazmul162001 <nazmulprime162001@gmail.com>
2022-05-05 01:37:47 +0600

    fixed server error

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 //middleware//
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 //user
 //foodieStore
@@ -17,29 +17,36 @@ app.use(express.json())
 //foodStore
 //9aQdPEAHjVwdyQ5B
 
-
-
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ehy4q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
-
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log('my db connected');
-  
-  
-  // perform actions on the collection object
-  client.close();
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
 });
 
+async function run() {
+  try {
+    await client.connect();
+    const foodCollection = client.db('foodStore').collection('service');
 
+    app.get('/service', async (req, res) => {
+      const query = {};
+      const cursor = foodCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+
+  } 
+  finally {
+  }
+}
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('Running My Foodie Store');
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
