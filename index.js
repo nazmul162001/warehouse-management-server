53cee466be8f9a5d7dd99bce88a5b593bc7163a7
nazmul162001 <nazmulprime162001@gmail.com>
2022-09-12 20:12:04 +0600

    fixed project path location

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function verifyJWT(req, res, next){
   if(!authHeader){
     return res.status(401).send({message: 'unauthorized access'});
   }
-  // verify Token
+  // verify Token //
   const token = authHeader.split(' ')[1];
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded)=>{
     if(err){
